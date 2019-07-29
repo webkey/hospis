e02a3cf31a34400dfecdcea9de5e93c64e82da7e
webkey <serhiy_i@ukr.net>
2019-07-29 16:36:21 +0300

    Первый коммит. Старт.

---
+++ src/js/common.js
@@ -0,0 +1,141 @@
+/**
+ * !Detects overlay scrollbars (when scrollbars on overflowed blocks are visible).
+ * This is found most commonly on mobile and OS X.
+ * */
+var HIDDEN_SCROLL = Modernizr.hiddenscroll;
+var NO_HIDDEN_SCROLL = !HIDDEN_SCROLL;
+
+/**
+ * !Add touchscreen classes
+ * */
+function addTouchClasses() {
+  if (!("ontouchstart" in document.documentElement)) {
+    document.documentElement.className += " no-touchevents";
+  } else {
+    document.documentElement.className += " touchevents";
+  }
+}
+
+/**
+ * !Add placeholder for old browsers
+ * */
+function placeholderInit() {
+  $('[placeholder]').placeholder();
+}
+
+/**
+ * !Add classes to form elements
+ * if they has a value or they are in focus
+ * */
+function formElementState() {
+  var $elem = $('.field-js');
+  
+  if ($elem.length) {
+    function toggleStateClass(mod, cond) {
+      var $this = $(this);
+      $this.add($this.prev('label')).toggleClass(mod, cond);
+    }
+
+    // Focus
+    $elem.on('focus blur', function (e) {
+      toggleStateClass.call(this, 'focused', e.handleObj.origType === "focus");
+    });
+
+    // Has value
+    $.each($elem, function () {
+      toggleStateClass.call(this, 'filled', $(this).val().length !== 0);
+    });
+
+    $elem.on('keyup change', function () {
+      toggleStateClass.call(this, 'filled', $(this).val().length !== 0);
+    });
+  }
+}
+
+/**
+ * !Initial custom select for cross-browser styling
+ * */
+function customSelect() {
+  var $select = $('select.cselect');
+
+  if ($select.length) {
+    $.each($select, function () {
+      var $thisSelect = $(this);
+      $thisSelect.select2({
+        theme: 'custom',
+        language: 'ru',
+        width: '100%',
+        containerCssClass: 'cselect-head',
+        dropdownCssClass: 'cselect-drop'
+      });
+    })
+
+  }
+}
+
+/**
+ * !Form validation
+ * */
+function formValidation() {
+  $.validator.setDefaults({
+    submitHandler: function() {
+      alert('Форма находится в тестовом режиме. Чтобы закрыть окно, нажмите ОК.');
+      return false;
+    }
+  });
+
+  var $form = $('.validate-js');
+
+  if ($form.length) {
+    var changeClasses = function (elem, remove, add) {
+      // console.log('changeClasses');
+      elem
+          .removeClass(remove).addClass(add);
+      elem
+          .closest('form').find('label[for="' + elem.attr('id') + '"]')
+          .removeClass(remove).addClass(add);
+      elem
+          .closest('.input-wrap')
+          .removeClass(remove).addClass(add);
+    };
+
+    $.each($form, function (index, element) {
+      $(element).validate({
+        errorClass: "error",
+        validClass: "success",
+        errorElement: false,
+        errorPlacement: function (error, element) {
+          return true;
+        },
+        highlight: function (element, errorClass, successClass) {
+          changeClasses($(element), successClass, errorClass);
+        },
+        unhighlight: function (element, errorClass, successClass) {
+          changeClasses($(element), errorClass, successClass);
+        }
+      });
+    });
+  }
+}
+
+/**
+ * =========== !ready document, load/resize window ===========
+ */
+
+$(window).on('load', function () {
+  // add functions
+});
+
+$(window).on('debouncedresize', function () {
+  // $(document.body).trigger("sticky_kit:recalc");
+});
+
+$(document).ready(function () {
+  addTouchClasses();
+  placeholderInit();
+  formElementState();
+  customSelect();
+  objectFitImages(); // object-fit-images initial
+
+  formValidation();
+});
